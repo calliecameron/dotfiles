b84c737ffdf5d7ac338f83c386cfb79f1ce32bf1
Callum Cameron <cjcameron7@gmail.com>
2016-06-15 11:45:32 +0100

    OS packages.

--- packages/02-os-packages/packages-main-gui.sh
+++ packages/02-os-packages/packages-main-gui.sh
@@ -6,7 +6,7 @@ sudo add-apt-repository -y ppa:nilarimogard/webupd8 &&
 sudo add-apt-repository -y ppa:stebbins/handbrake-releases &&
 sudo apt-get update &&
 
-sudo apt-get -y install biber dconf-tools dia dia-shapes dictd dict-foldoc dict-gcide dict-jargon dict-wn emacs24 emacs24-el emacs-goodies-el epstool fonts-crosextra-caladea fonts-crosextra-carlito freefilesync gimp gnucash gnucash-docs handbrake-gtk kdelibs5-plugins kdelibs5-data manpages-posix manpages-posix-dev mosh octave okular oxygen-icon-theme python-nemo qtbase5-dev sqlite3 texlive-full transfig ttf-ancient-fonts ttf-mscorefonts-installer ubuntu-restricted-extras unoconv wmctrl workrave xclip xsane || exit 1
+sudo apt-get -y install biber dconf-tools dia dia-shapes dictd dict-foldoc dict-gcide dict-jargon dict-wn emacs24 emacs24-el emacs-goodies-el epstool fonts-crosextra-caladea fonts-crosextra-carlito freefilesync gimp gnucash gnucash-docs handbrake-gtk kdelibs5-plugins kdelibs5-data manpages-posix manpages-posix-dev mosh octave okular oxygen-icon-theme python-dev python3-dev python-nemo qtbase5-dev sqlite3 texlive-full transfig ttf-ancient-fonts ttf-mscorefonts-installer ubuntu-restricted-extras unoconv wmctrl workrave xclip xsane || exit 1
 
 if is64bit -q; then
     if ! which google-chrome &>/dev/null; then
@@ -18,7 +18,7 @@ if is64bit -q; then
         rmdir "${TMPDIR}" || exit 1
     fi
 else
-    sudo apt-get install chromium-browser || exit 1
+    sudo apt-get -y install chromium-browser || exit 1
 fi
 
 sudo pip install pyinotify &&
