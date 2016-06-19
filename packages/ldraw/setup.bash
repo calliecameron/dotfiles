554ef571400d0ee1d9309893a4a2a8d2649fa05e
Callum Cameron <cjcameron7@gmail.com>
2016-06-19 11:26:42 +0100

    Added ldraw package.

---
+++ packages/ldraw/setup.bash
@@ -0,0 +1,34 @@
+function _can-install() {
+    [ "${DOTFILES_OS}" = 'linux' ] &&
+    [ "${DOTFILES_LINUX_VARIANT}" = 'main' ] &&
+    [ ! -z "${DOTFILES_CAN_SUDO}" ]
+}
+
+function _install() {
+    sudo apt-get -y install wine wine-gecko2.21 wine-mono0.0.8 gnome-exe-thumbnailer &&
+    mkdir -p "${PACKAGE_INSTALL_DIR}" &&
+    cd "${PACKAGE_INSTALL_DIR}" &&
+    wget -O complete.zip 'http://www.ldraw.org/library/updates/complete.zip' &&
+    unzip complete.zip &&
+    cd ldraw &&
+    wget -O mlcad.zip 'http://mlcad.lm-software.com/MLCad_V3.40.zip' &&
+    unzip mlcad.zip &&
+    chmod u+x MLCad_V3.40/MLCAD.exe &&
+    wget -O make-list 'https://raw.githubusercontent.com/nathaneltitane/ldraw/master/make-list' &&
+    chmod u+x make-list &&
+    ./make-list -d &&
+    mkdir -p "${PACKAGE_INSTALL_DIR}/bin" &&
+    cp "${PACKAGE_CONF_DIR}/mlcad" "${PACKAGE_INSTALL_DIR}/bin/mlcad" &&
+    sed "s|@@@@@1@@@@@|${PACKAGE_CONF_DIR}/logo.png|g" < "${PACKAGE_CONF_DIR}/mlcad-desktop-template" > "${PACKAGE_CONF_DIR}/mlcad.desktop" &&
+    chmod u+x "${PACKAGE_CONF_DIR}/mlcad.desktop" &&
+    xdg-desktop-menu install --novendor "${PACKAGE_CONF_DIR}/mlcad.desktop" &&
+    xdg-desktop-icon install --novendor "${PACKAGE_CONF_DIR}/mlcad.desktop"
+}
+
+function _update() {
+    cd "${PACKAGE_INSTALL_DIR}" &&
+    wget -O complete.zip 'http://www.ldraw.org/library/updates/complete.zip' &&
+    unzip complete.zip &&
+    cd ldraw &&
+    ./make-list -d
+}
