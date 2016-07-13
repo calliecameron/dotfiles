a8839cf21f6e787ee72f7a08a27702db46f7f5b3
Callum Cameron <cjcameron7@gmail.com>
2016-07-13 14:49:57 +0100

    IPython settings.

--- packages/python/ipython_config.py
+++ packages/python/ipython_config.py
@@ -1,4 +1,4 @@
 c = get_config()
 
 c.InteractiveShellApp.extensions = ["autoreload"]
-c.InteractiveShellApp.exec_lines = ["%autoreload 2"]
+c.InteractiveShellApp.exec_lines = ["%autoreload 2", "%matplotlib"]
