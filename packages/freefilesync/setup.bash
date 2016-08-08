0d80515776486ff42141e5fffcbe4908666526ec
Callum Cameron <cjcameron7@gmail.com>
2016-08-08 08:53:44 +0100

    Fixed freefilesync download.

--- packages/freefilesync/setup.bash
+++ packages/freefilesync/setup.bash
@@ -8,7 +8,7 @@ function _install() {
     # This is temporary, until the PPA supports xenial
     mkdir -p "${PACKAGE_INSTALL_DIR}" &&
     cd "${PACKAGE_INSTALL_DIR}" &&
-    wget http://download1208.mediafire.com/eo3eqyx1qpug/i7ral2ixb6xx2x2/FreeFileSync_8.3_Ubuntu_16.04_64-bit.tar.gz &&
+    wget http://www.mediafire.com/download/i7ral2ixb6xx2x2/FreeFileSync_8.3_Ubuntu_16.04_64-bit.tar.gz &&
     tar -xf FreeFileSync_8.3_Ubuntu_16.04_64-bit.tar.gz &&
     cd FreeFileSync &&
     chmod a-x CHANGELOG LICENSE Resources.zip ding.wav gong.wav harp.wav
