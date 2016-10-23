b34b0c39e57b7cb0c2aac4ec1e2e6dde31eee99d
Callum Cameron <cjcameron7@gmail.com>
2016-10-23 11:58:15 +0200

    More emacs25 stuff.

--- core/default-dotfiles/default-emacs.el
+++ core/default-dotfiles/default-emacs.el
@@ -1,4 +1,8 @@
 ;; -*-Emacs-Lisp-*-
+
+;; Emacs 25 wants this here, even though we call it later...
+(package-initialize)
+
 (let ((dotfiles-load-path (getenv "DOTFILES_CORE_DIR")))
   (if dotfiles-load-path
       (progn
