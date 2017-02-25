a6c3f17bc54f0998c86f40ac5ab99bddd9210760
Callum Cameron <cjcameron7@gmail.com>
2017-02-25 15:59:14 +0100

    Updated Tomb version.

--- packages/tomb/setup-tomb.sh
+++ packages/tomb/setup-tomb.sh
@@ -4,9 +4,9 @@ sudo apt-get -y install cryptsetup dcfldd gnupg pinentry-gtk2 steghide &&
 
 BUILD_DIR="$(mktemp -d)" &&
 cd "${BUILD_DIR}" &&
-wget 'https://files.dyne.org/tomb/tomb-2.2.tar.gz' &&
-tar -xf 'tomb-2.2.tar.gz' &&
-cd 'tomb-2.2' &&
+wget 'https://files.dyne.org/tomb/Tomb-2.3.tar.gz' &&
+tar -xf 'Tomb-2.3.tar.gz' &&
+cd 'Tomb-2.3' &&
 sudo make install &&
 cd &&
 rm -rf "${BUILD_DIR}"
