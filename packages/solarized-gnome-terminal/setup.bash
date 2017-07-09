34240140e35093804ef382d67a4b7dc5df6e5603
Callum Cameron <cjcameron7@gmail.com>
2017-07-09 12:07:31 +0200

    Updated solarized-gnome-terminal and tomb.

--- packages/solarized-gnome-terminal/setup.bash
+++ packages/solarized-gnome-terminal/setup.bash
@@ -4,7 +4,7 @@ function _can-install() {
 
 function _install() {
     git clone https://github.com/Anthony25/gnome-terminal-colors-solarized.git "${PACKAGE_INSTALL_DIR}" || return 1
-    if ! "${PACKAGE_INSTALL_DIR}/install.sh" --scheme light --profile Default; then
+    if ! "${PACKAGE_INSTALL_DIR}/install.sh" --scheme light --profile Default --skip-dircolors; then
         echo-red "If this fails to install, change the profile name in Gnome Terminal to 'Default'"
         return 1
     fi
@@ -13,5 +13,5 @@ function _install() {
 
 function _update() {
     git pull &&
-    "${PACKAGE_INSTALL_DIR}/install.sh" --scheme light --profile Default
+    "${PACKAGE_INSTALL_DIR}/install.sh" --scheme light --profile Default --skip-dircolors
 }
--- packages/tomb/setup-tomb.sh
+++ packages/tomb/setup-tomb.sh
@@ -4,9 +4,9 @@ sudo apt-get -y install cryptsetup dcfldd gnupg pinentry-gtk2 steghide &&
 
 BUILD_DIR="$(mktemp -d)" &&
 cd "${BUILD_DIR}" &&
-wget 'https://files.dyne.org/tomb/Tomb-2.3.tar.gz' &&
-tar -xf 'Tomb-2.3.tar.gz' &&
-cd 'Tomb-2.3' &&
+wget 'https://files.dyne.org/tomb/Tomb-2.4.tar.gz' &&
+tar -xf 'Tomb-2.4.tar.gz' &&
+cd 'Tomb-2.4' &&
 sudo make install &&
 cd &&
 rm -rf "${BUILD_DIR}"
