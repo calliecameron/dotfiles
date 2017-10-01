fbc93609a5515a1d10b114a71aa70a40d4f31513
Callum Cameron <cjcameron7@gmail.com>
2017-10-01 19:41:55 +0200

    Make solarized-gnome-terminal work again.

--- packages/solarized-gnome-terminal/setup.bash
+++ packages/solarized-gnome-terminal/setup.bash
@@ -3,15 +3,19 @@ function _can-install() {
 }
 
 function _install() {
-    git clone https://github.com/Anthony25/gnome-terminal-colors-solarized.git "${PACKAGE_INSTALL_DIR}" || return 1
-    if ! "${PACKAGE_INSTALL_DIR}/install.sh" --scheme light --profile Default --skip-dircolors; then
-        echo-red "If this fails to install, change the profile name in Gnome Terminal to 'Default'"
+    if [ -z "${DOTFILES_GNOME_TERMINAL_PROFILE}" ]; then
+        echo-red "Set DOTFILES_GNOME_TERMINAL_PROFILE to the profile ID reported by Gnome Terminal to install."
         return 1
     fi
-    return 0
+    git clone https://github.com/Anthony25/gnome-terminal-colors-solarized.git "${PACKAGE_INSTALL_DIR}" &&
+    "${PACKAGE_INSTALL_DIR}/install.sh" --scheme light --profile "${DOTFILES_GNOME_TERMINAL_PROFILE}" --skip-dircolors
 }
 
 function _update() {
+    if [ -z "${DOTFILES_GNOME_TERMINAL_PROFILE}" ]; then
+        echo-red "Set DOTFILES_GNOME_TERMINAL_PROFILE to the profile ID reported by Gnome Terminal to update."
+        return 1
+    fi
     git pull &&
-    "${PACKAGE_INSTALL_DIR}/install.sh" --scheme light --profile Default --skip-dircolors
+    "${PACKAGE_INSTALL_DIR}/install.sh" --scheme light --profile "${DOTFILES_GNOME_TERMINAL_PROFILE}" --skip-dircolors
 }
