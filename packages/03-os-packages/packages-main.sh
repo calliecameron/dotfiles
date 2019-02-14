8c9efaf0f5daadeff1645706ca0f6b44684f9fa4
Callum Cameron <cjcameron7@gmail.com>
2019-02-14 08:11:05 +0100

    Some tweaks.

--- packages/03-os-packages/packages-main.sh
+++ packages/03-os-packages/packages-main.sh
@@ -4,6 +4,7 @@
 sudo apt-get update &&
 sudo apt-get autoremove pix hexchat samba-common samba-libs thunderbird transmission-gtk &&
 
-sudo apt-get -y install curl fonts-crosextra-caladea fonts-crosextra-carlito git inotify-tools laptop-detect mercurial nano ncurses-term netcat-openbsd powertop screen sqlite3 tree ttf-ancient-fonts ttf-mscorefonts-installer ubuntu-restricted-extras xclip zsh zsh-doc &&
+sudo apt-get -y install apparmor-profiles apparmor-utils curl fonts-crosextra-caladea fonts-crosextra-carlito git inotify-tools laptop-detect mercurial nano ncurses-term netcat-openbsd powertop screen sqlite3 tree ttf-ancient-fonts ttf-mscorefonts-installer ubuntu-restricted-extras xclip zsh zsh-doc &&
 
+sudo rm -f /etc/sudoers.d/0pwfeedback
 fc-cache
--- packages/avahi-daemon/setup-avahi.sh
+++ packages/avahi-daemon/setup-avahi.sh
@@ -2,6 +2,9 @@
 
 source "${DOTFILES_BASH_COMMON}" || exit 1
 
+sudo aa-enforce /etc/apparmor.d/usr.sbin.avahi-daemon &&
+sudo systemctl restart avahi-daemon.service || exit 1
+
 if yn-n "Open firewall port for Avahi daemon (local name resolution) (UDP port 5353)?"; then
     port open-at-boot 5353 udp &&
     port open 5353 udp || exit 1
