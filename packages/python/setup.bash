d9e01e7a195340bc14d942814a204218b0d3faa1
Callum Cameron <cjcameron7@gmail.com>
2019-02-14 22:47:34 +0100

    Fix python.

--- packages/python/setup.bash
+++ packages/python/setup.bash
@@ -5,11 +5,12 @@ function _install() {
 function _install() {
     sudo apt-get -y install python-dev python-pip python3-dev python3-pip &&
     # Make sure we're using the local pip if it exists, not the apt-installed one
-    export PATH="${HOME}/.local/bin:${PATH}" &&
-    pip install --user --upgrade pip setuptools &&
-    pip3 install --user --upgrade pip setuptools &&
-    pip install --user --upgrade future gitpython ipython pyinotify pytimeparse rlipython &&
-    pip3 install --user --upgrade autoenv autopep8 future gitpython flake8 importmagic ipython jedi mypy pygments pyinotify pylint pytimeparse pep8 rlipython rope virtualenv virtualenvwrapper yapf
+    LOCAL_PIP_DIR="${HOME}/.local/bin" &&
+    export PATH="${LOCAL_PIP_DIR}:${PATH}" &&
+    "${LOCAL_PIP_DIR}/pip" install --user --upgrade pip setuptools &&
+    "${LOCAL_PIP_DIR}/pip3" install --user --upgrade pip setuptools &&
+    "${LOCAL_PIP_DIR}/pip" install --user --upgrade future gitpython ipython pyinotify pytimeparse rlipython &&
+    "${LOCAL_PIP_DIR}/pip3" install --user --upgrade autoenv autopep8 future gitpython flake8 importmagic ipython jedi mypy pygments pyinotify pylint pytimeparse pep8 rlipython rope virtualenv virtualenvwrapper yapf
 }
 
 function _update() {
