1d299ac096c51f55fcfb72acdedf01c673d9898c
Callum Cameron <cjcameron7@gmail.com>
2020-03-28 16:43:33 +0100

    Update virtualbox.

--- packages/vm-tools/setup.bash
+++ packages/vm-tools/setup.bash
@@ -9,7 +9,7 @@ function _install() {
     echo deb https://download.virtualbox.org/virtualbox/debian bionic contrib | sudo tee /etc/apt/sources.list.d/virtualbox.list &&
     wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | sudo apt-key add - &&
     sudo apt-get update &&
-    sudo apt-get -y install virtualbox-6.0 &&
+    sudo apt-get -y install virtualbox-6.1 &&
     sudo adduser "$(id -un)" vboxusers &&
 
     # Vagrant
