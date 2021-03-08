b6c7f8f37e0b6eab60e3a90a0800748312c1c8aa
Callum Cameron <cjcameron7@gmail.com>
2021-03-08 19:14:50 +0100

    Update pandoc version.

--- packages/text-tools/setup.bash
+++ packages/text-tools/setup.bash
@@ -3,5 +3,15 @@ function _can-install() {
 }
 
 function _install() {
-    sudo apt-get -y install asciidoc aspell biber chktex dict dictd dict-foldoc dict-gcide dict-jargon dict-wn epstool hunspell hunspell-en-gb markdown pandoc texinfo texlive-full transfig unoconv
+    sudo apt-get -y install asciidoc aspell biber chktex dict dictd dict-foldoc dict-gcide dict-jargon dict-wn epstool hunspell hunspell-en-gb markdown texinfo texlive-full transfig unoconv || exit 1
+
+    if linux-variant main; then
+        if ! which pandoc &>/dev/null; then
+            TMPDIR="$(mktemp -d)" &&
+            wget -O "${TMPDIR}/pandoc.deb" https://github.com/jgm/pandoc/releases/download/2.11.4/pandoc-2.11.4-1-amd64.deb &&
+            sudo dpkg -i "${TMPDIR}/pandoc.deb" &&
+            rm "${TMPDIR}/pandoc.deb" &&
+            rmdir "${TMPDIR}" || exit 1
+        fi
+    fi
 }
