0ffbb296708f52198b3e577de83daac564c72a0f
Callum Cameron <cjcameron7@gmail.com>
2021-04-10 15:06:07 +0200

    Upgrade to pandoc 2.13.

--- packages/text-tools/setup.bash
+++ packages/text-tools/setup.bash
@@ -14,7 +14,7 @@ function _install() {
     if linux-variant main; then
         if ! which pandoc &>/dev/null; then
             TMPDIR="$(mktemp -d)" &&
-            wget -O "${TMPDIR}/pandoc.deb" https://github.com/jgm/pandoc/releases/download/2.11.4/pandoc-2.11.4-1-amd64.deb &&
+            wget -O "${TMPDIR}/pandoc.deb" https://github.com/jgm/pandoc/releases/download/2.13/pandoc-2.13-1-amd64.deb &&
             sudo dpkg -i "${TMPDIR}/pandoc.deb" &&
             rm "${TMPDIR}/pandoc.deb" &&
             rmdir "${TMPDIR}" || exit 1
