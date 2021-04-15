d7c88575afeb3dcd12ed7a19ecd8f5e50c3fb14c
Callie Cameron <cjcameron7@gmail.com>
2021-04-15 17:39:02 +0200

    Update anki.

--- packages/anki/setup.bash
+++ packages/anki/setup.bash
@@ -6,13 +6,13 @@ function _install() {
     mkdir "${PACKAGE_INSTALL_DIR}" "${PACKAGE_INSTALL_DIR}/bin" &&
     cd "${PACKAGE_INSTALL_DIR}" &&
     curl https://raw.githubusercontent.com/dae/anki/master/pkgkey.asc | gpg --import &&
-    wget https://apps.ankiweb.net/downloads/current/anki-2.1.8-checksums.txt &&
-    gpg --verify anki-2.1.8-checksums.txt &&
-    wget 'https://apps.ankiweb.net/downloads/current/anki-2.1.8-linux-amd64.tar.bz2' &&
+    wget https://github.com/ankitects/anki/releases/download/2.1.43/anki-2.1.43-checksums.txt &&
+    gpg --verify anki-2.1.43-checksums.txt &&
+    wget https://github.com/ankitects/anki/releases/download/2.1.43/anki-2.1.43-linux.tar.bz2 &&
     TMPFILE="$(mktemp)" &&
-    grep 'anki-2.1.8-linux-amd64.tar.bz2' anki-2.1.8-checksums.txt > "${TMPFILE}" &&
+    grep 'anki-2.1.43-linux.tar.bz2' anki-2.1.43-checksums.txt > "${TMPFILE}" &&
     sha256sum -c "${TMPFILE}" &&
-    tar -xf 'anki-2.1.8-linux-amd64.tar.bz2' &&
-    ln -s "${PACKAGE_INSTALL_DIR}/anki-2.1.8/bin/anki" "${PACKAGE_INSTALL_DIR}/bin/anki" &&
-    rm 'anki-2.1.8-linux-amd64.tar.bz2' "${TMPFILE}"
+    tar -xf 'anki-2.1.43-linux.tar.bz2' &&
+    ln -s "${PACKAGE_INSTALL_DIR}/anki-2.1.43-linux/bin/Anki" "${PACKAGE_INSTALL_DIR}/bin/anki" &&
+    rm 'anki-2.1.43-linux.tar.bz2' "${TMPFILE}"
 }
