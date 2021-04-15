b6eb654e4a6c706219be93168e7b391c822f7142
Callie Cameron <cjcameron7@gmail.com>
2021-04-15 18:18:08 +0200

    Update dropbox.

--- packages/dropbox-gui/setup-dropbox-gui.sh
+++ packages/dropbox-gui/setup-dropbox-gui.sh
@@ -2,7 +2,7 @@
 
 source "${DOTFILES_BASH_COMMON}" &&
 
-sudo apt-get -y install python-gpg nemo-dropbox || exit 1
+sudo apt-get -y install nemo-dropbox || exit 1
 
 if yn-n "Open firewall port for Dropbox LAN sync (TCP port 17500)?"; then
     port open-at-boot 17500 tcp &&
