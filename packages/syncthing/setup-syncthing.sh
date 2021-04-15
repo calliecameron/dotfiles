9d2f104a4db57307a79da14e5b465a59047add4c
Callie Cameron <cjcameron7@gmail.com>
2021-04-15 18:18:24 +0200

    Update syncthing.

--- packages/syncthing/setup-syncthing.sh
+++ packages/syncthing/setup-syncthing.sh
@@ -4,6 +4,7 @@ source "${DOTFILES_BASH_COMMON}" &&
 
 curl -s https://syncthing.net/release-key.txt | sudo apt-key add - &&
 echo 'deb https://apt.syncthing.net/ syncthing stable' | sudo tee /etc/apt/sources.list.d/syncthing-release.list &&
+printf "Package: *\nPin: origin apt.syncthing.net\nPin-Priority: 990\n" | sudo tee /etc/apt/preferences.d/syncthing &&
 
 curl -s https://download.opensuse.org/repositories/home:/kozec/xUbuntu_18.04/Release.key | sudo apt-key add - &&
 echo 'deb https://download.opensuse.org/repositories/home:/kozec/xUbuntu_18.04/ /' | sudo tee /etc/apt/sources.list.d/syncthing-gtk.list &&
