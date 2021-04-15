ce3013e50303c6bc597d4e7560322a12ea072b4c
Callie Cameron <cjcameron7@gmail.com>
2021-04-15 17:49:43 +0200

    Update python.

--- packages/python/setup.bash
+++ packages/python/setup.bash
@@ -3,13 +3,16 @@ function _install() {
 }
 
 function _install() {
-    sudo apt-get -y install python-dev python-pip python3-dev python3-pip &&
+    sudo apt-get -y install python3-dev python3-pip &&
     # Make sure we're using the local pip if it exists, not the apt-installed one
     LOCAL_PIP_DIR="${HOME}/.local/bin" &&
     export PATH="${LOCAL_PIP_DIR}:${PATH}" &&
-    "${LOCAL_PIP_DIR}/pip" install --user --upgrade pip setuptools &&
-    "${LOCAL_PIP_DIR}/pip3" install --user --upgrade pip setuptools &&
-    "${LOCAL_PIP_DIR}/pip" install --user --upgrade future gitpython ipython pyinotify pytimeparse rlipython &&
+    if [ -f "${LOCAL_PIP_DIR}/pip3" ]; then
+        BASE_PIP="${LOCAL_PIP_DIR}/pip3"
+    else
+        BASE_PIP='pip3'
+    fi
+    "${BASE_PIP}" install --user --upgrade pip setuptools wheel &&
     "${LOCAL_PIP_DIR}/pip3" install --user --upgrade autoenv autopep8 future gitpython flake8 importmagic 'ipython<7' jupyter 'jupyter-console<6' jedi mypy pandoc-include pdfminer pygments pyinotify pylint pytimeparse pep8 rlipython rope snakeviz virtualenv virtualenvwrapper yapf
 }
 
