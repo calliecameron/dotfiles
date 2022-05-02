eb4c97393cb7de7cc2cbd1a8e2eaa740c646b396
Callie Cameron <cjcameron7@gmail.com>
2022-05-02 22:09:01 +0200

    Add bats.

--- packages/dev-tools/env.sh
+++ packages/dev-tools/env.sh
@@ -1,3 +1,5 @@
 homebinlink ccache gcc g++ cc c++
 homebinlink ctags-exuberant ctags etags
 export SHELLCHECK_OPTS="-e SC1090 -e SC1091"
+BATS_LIB_PATH="$(readlink -f "$(dirname "$(nvm which current)")/../lib/node_modules")"
+export BATS_LIB_PATH
--- packages/dev-tools/setup.bash
+++ packages/dev-tools/setup.bash
@@ -1,7 +1,13 @@
 function _can-install() {
-    os linux && linux-variant main pi && can-sudo
+    # Depends on antigen for npm
+    os linux && linux-variant main pi && can-sudo && package-installed antigen
 }
 
 function _install() {
-    sudo apt-get -y install bash-doc bison bison-doc build-essential ccache clang clang-format cmake cppcheck devscripts exuberant-ctags flex global info make-doc man-db manpages-posix manpages-posix-dev shellcheck valgrind
+    sudo apt-get -y install bash-doc bison bison-doc build-essential ccache clang clang-format cmake cppcheck devscripts exuberant-ctags flex global info make-doc man-db manpages-posix manpages-posix-dev shellcheck valgrind &&
+    npm install -g bats bats-support bats-assert bats-file
+}
+
+function _update() {
+    _install
 }
