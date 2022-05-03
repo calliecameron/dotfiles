14be4c1b635831bf2f85ed8fc29e1de150a76ffd
Callie Cameron <cjcameron7@gmail.com>
2022-05-03 20:22:31 +0200

    Remove bats-file.

--- packages/dev-tools/setup.bash
+++ packages/dev-tools/setup.bash
@@ -5,7 +5,7 @@ function _can-install() {
 
 function _install() {
     sudo apt-get -y install bash-doc bison bison-doc build-essential ccache clang clang-format cmake cppcheck devscripts exuberant-ctags flex global info make-doc man-db manpages-posix manpages-posix-dev shellcheck valgrind &&
-    npm install -g bats bats-support bats-assert bats-file
+    npm install -g bats bats-support bats-assert
 }
 
 function _update() {
