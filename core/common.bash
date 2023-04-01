eda1430b7cb87b9d2c7201a2134a5d597c02abd7
Callie Cameron <cjcameron7@gmail.com>
2023-04-01 10:04:01 +0200

    Factor out port functions.

--- core/common.bash
+++ core/common.bash
@@ -5,22 +5,3 @@
 # top-level script) -- but that should never be done anyway!
 # shellcheck disable=SC2034
 THIS_DIR="$(cd "$(dirname "${BASH_SOURCE[1]}")" && pwd)"
-
-function valid-protocol() {
-    if [ "${1}" = 'tcp' ] || [ "${1}" = 'udp' ]; then
-        return 0
-    else
-        return 1
-    fi
-}
-
-function valid-port() {
-    if [ -n "${1}" ]; then
-        if [[ "${1}" != *[!0-9]* ]]; then
-            if (("${1}" >= 0 && "${1}" <= 65535)); then
-                return 0
-            fi
-        fi
-    fi
-    return 1
-}
