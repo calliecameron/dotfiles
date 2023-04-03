c1ed3bb90447c35f8c769d30d80e1a862e586612
Callie Cameron <cjcameron7@gmail.com>
2023-04-03 19:52:49 +0200

    Lint.

--- tests/find-shell-files.sh
+++ tests/find-shell-files.sh
@@ -3,10 +3,12 @@
 set -eu
 
 git ls-files | LC_ALL=C sort | while read -r line; do
-    HEAD="$(head -n 1 "${line}")"
-    if [[ "${line}" == *'.sh' ]] || [[ "${line}" == *'.bash' ]] ||
-        [[ "${line}" == *'.bats' ]] ||
-        [ "${HEAD}" = '#!/bin/sh' ] || [ "${HEAD}" = '#!/bin/bash' ]; then
-        echo "${line}"
+    if [[ "${line}" != *'git-completion.bash' ]]; then
+        HEAD="$(head -n 1 "${line}")"
+        if [[ "${line}" == *'.sh' ]] || [[ "${line}" == *'.bash' ]] ||
+            [[ "${line}" == *'.bats' ]] ||
+            [ "${HEAD}" = '#!/bin/sh' ] || [ "${HEAD}" = '#!/bin/bash' ]; then
+            echo "${line}"
+        fi
     fi
 done
