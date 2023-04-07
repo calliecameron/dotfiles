08f268fc9880666167af260cabf759e447f8d7d4
Callie Cameron <cjcameron7@gmail.com>
2023-04-07 11:34:39 +0200

    Update dev-tools.

--- packages/dev-tools/env.sh
+++ packages/dev-tools/env.sh
@@ -1,5 +1,7 @@
+# shellcheck shell=sh
+
 dotfiles-home-bin-link ccache gcc g++ cc c++
 dotfiles-home-bin-link ctags-exuberant ctags etags
-export SHELLCHECK_OPTS="-e SC1090 -e SC1091"
+
 BATS_LIB_PATH="$(readlink -f "$(dirname "$(nvm which current)")/../lib/node_modules")"
 export BATS_LIB_PATH
