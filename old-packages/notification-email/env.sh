e930ae97ece55db13b18b64f4bffca6f2b75824b
Callie Cameron <cjcameron7@gmail.com>
2023-04-07 15:02:31 +0200

    Update notification-email.

--- old-packages/notification-email/env.sh
+++
@@ -1,5 +0,0 @@
-export DOTFILES_SSMTP_CONFIG="${HOME}/.dotfiles-ssmtp-config"
-
-if [ ! -e "${DOTFILES_SSMTP_CONFIG}" ]; then
-    cp "${PACKAGE_SOURCE_DIR}/ssmtp-conf-template" "${DOTFILES_SSMTP_CONFIG}"
-fi
--- packages/notification-email/aliases.sh
+++ packages/notification-email/aliases.sh
@@ -1,2 +1,4 @@
+# shellcheck shell=bash
+
 complainunset 'DOTFILES_EMAIL_FROM' 'From address for notification emails'
 complainunset 'DOTFILES_EMAIL_TO' 'Target address for notification emails'
