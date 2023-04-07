bc60b847944ac1574eead8ff85c80273e8e0a46e
Callie Cameron <cjcameron7@gmail.com>
2023-04-07 14:42:09 +0200

    Update vscode.

--- packages/vscode/env.sh
+++ packages/vscode/env.sh
@@ -1,2 +1,4 @@
+# shellcheck shell=sh
+
 dotfiles-home-link "${PACKAGE_SOURCE_DIR}/settings.json" "${HOME}/.config/Code/User/settings.json"
 dotfiles-home-link "${PACKAGE_SOURCE_DIR}/keybindings.json" "${HOME}/.config/Code/User/keybindings.json"
