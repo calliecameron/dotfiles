6d23eb77429600d2b293b834c4ce04e25278a09e
Callie Cameron <cjcameron7@gmail.com>
2023-04-07 17:31:17 +0200

    Remove go.

--- packages/go/aliases.sh
+++
@@ -1,6 +0,0 @@
-# shellcheck shell=bash
-
-if ! gvm list | grep "${DOTFILES_GVM_GO_VERSION}" >/dev/null 2>/dev/null; then
-    gvm install "${DOTFILES_GVM_GO_VERSION}" --prefer-binary &&
-        gvm use "${DOTFILES_GVM_GO_VERSION}"
-fi
