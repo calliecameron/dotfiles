85eabef7b19046570a4a475eaf8d703577c1e9df
Callie Cameron <cjcameron7@gmail.com>
2023-04-07 11:53:21 +0200

    Update dev-tools.

--- packages/antigen/env.sh
+++ packages/antigen/env.sh
@@ -9,5 +9,5 @@ if [ -f "${NVM_DIR}/nvm.sh" ]; then
     . "${NVM_DIR}/nvm.sh" && nvm use node
 fi
 
-DOTFILES_NVM_LIB_PATH="$(zsh -c "PACKAGE_INSTALL_DIR=\"${PACKAGE_INSTALL_DIR}\" && source \"${PACKAGE_SOURCE_DIR}/aliases.zsh\" && readlink -f \"\$(dirname \"$(nvm which current)\")/../lib/node_modules)\"")"
+DOTFILES_NVM_LIB_PATH="$(zsh -c "PACKAGE_INSTALL_DIR=\"${PACKAGE_INSTALL_DIR}\" && source \"${PACKAGE_SOURCE_DIR}/aliases.zsh\" && readlink -f \"\$(dirname \"$(nvm which current)\")/../lib/node_modules\"")"
 export DOTFILES_NVM_LIB_PATH
