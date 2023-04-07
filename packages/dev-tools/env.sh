90cb4729878bf1474b8462b5a7330bad07b3cd3c
Callie Cameron <cjcameron7@gmail.com>
2023-04-07 11:51:28 +0200

    Update dev-tools.

--- packages/dev-tools/env.sh
+++ packages/dev-tools/env.sh
@@ -3,5 +3,4 @@
 dotfiles-home-bin-link ccache gcc g++ cc c++
 dotfiles-home-bin-link ctags-exuberant ctags etags
 
-BATS_LIB_PATH="$(readlink -f "$(dirname "$(nvm which current)")/../lib/node_modules")"
-export BATS_LIB_PATH
+export BATS_LIB_PATH="${DOTFILES_NVM_LIB_PATH}"
