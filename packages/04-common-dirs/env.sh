d98b4332c41f38b27428abc97cdf01175c7e3617
Callie Cameron <cjcameron7@gmail.com>
2023-09-16 11:45:47 +0200

    Fix home-link usage.

--- packages/04-common-dirs/env.sh
+++ packages/04-common-dirs/env.sh
@@ -1,9 +1,9 @@
 # shellcheck shell=sh
 
-dotfiles-home-link "${WINROOT}" "${HOME}/WinRoot"
-dotfiles-home-link "${WINHOME}" "${HOME}/WinHome"
-dotfiles-home-link "${WINDOWNLOADS}" "${HOME}/WinDownloads"
-dotfiles-home-link "${CYGHOME}" "${HOME}/CygHome"
+test -n "${WINROOT:-}" && dotfiles-home-link "${WINROOT}" "${HOME}/WinRoot"
+test -n "${WINHOME:-}" && dotfiles-home-link "${WINHOME}" "${HOME}/WinHome"
+test -n "${WINDOWNLOADS:-}" && dotfiles-home-link "${WINDOWNLOADS}" "${HOME}/WinDownloads"
+test -n "${CYGHOME:-}" && dotfiles-home-link "${CYGHOME}" "${HOME}/CygHome"
 
 if [ -z "${DROPBOX}" ] && [ -d "${HOME}/Dropbox" ] && [ ! -h "${HOME}/Dropbox" ]; then
     export DROPBOX="${HOME}/Dropbox"
