2ec725ad0d9df33be0f947628a943df89af27c86
Callie Cameron <cjcameron7@gmail.com>
2024-09-07 11:00:50 +0200

    Add nixGL.

--- packages/05-nix/env.sh
+++ packages/05-nix/env.sh
@@ -5,6 +5,7 @@ export DOTFILES_NIXPKGS_VERSION='24.05'
 export DOTFILES_NIXPKGS="nixpkgs/release-${DOTFILES_NIXPKGS_VERSION}"
 export DOTFILES_NIX_PROFILE_DIR="${PACKAGE_INSTALL_DIR}/profiles"
 export DOTFILES_NIX_PROFILE="${DOTFILES_NIX_PROFILE_DIR}/dotfiles-${DOTFILES_NIXPKGS_VERSION}"
+export LOCALE_ARCHIVE='/usr/lib/locale/locale-archive'
 
 dotfiles-home-link "${PACKAGE_SOURCE_DIR}/nix.conf" "${HOME}/.config/nix/nix.conf"
 
