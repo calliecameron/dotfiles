9e6bcfcce1537edfe21c53f58f432d069295a5b2
Callie Cameron <cjcameron7@gmail.com>
2025-02-10 19:20:16 +0100

    Update python for mint 22.

--- packages/python/env.sh
+++ packages/python/env.sh
@@ -4,9 +4,6 @@ if command -v pygmentize >/dev/null; then
     dotfiles-home-link "${PACKAGE_SOURCE_DIR}/lessfilter"
 fi
 
-# This is where pip --user installs binaries
-export PATH="${HOME}/.local/bin:${PATH}"
-
 export WORKON_HOME="${HOME}/.virtualenvs"
 mkdir -p "${WORKON_HOME}"
 VIRTUALENVWRAPPER_PYTHON="$(readlink -f "$(command -v python3)")"
