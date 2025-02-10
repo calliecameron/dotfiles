2a4332d87ef86a5146b043f3bb5ae73945ddd16d
Callie Cameron <cjcameron7@gmail.com>
2025-02-10 14:05:50 +0100

    Simplify bazel aliases.

--- packages/bazel/aliases.bash
+++
@@ -1,5 +0,0 @@
-if [ -z "${PROMPT_COMMAND}" ]; then
-    PROMPT_COMMAND='bazel-workspace-check'
-else
-    PROMPT_COMMAND="${PROMPT_COMMAND}; bazel-workspace-check"
-fi
--- packages/bazel/aliases.sh
+++ packages/bazel/aliases.sh
@@ -1,45 +1,6 @@
 # shellcheck shell=bash
 
-function bazel-workspace-root() {
-    local DIR
-    DIR="$(readlink -f .)"
-
-    while true; do
-        if [ -f "${DIR}/WORKSPACE" ]; then
-            echo "${DIR}"
-            return 0
-        elif [ "${DIR}" = '/' ]; then
-            return 0
-        else
-            DIR="$(readlink -f "${DIR}/..")"
-        fi
-    done
-}
-
-function bazel-workspace-check() {
-    local ROOT
-    ROOT="$(bazel-workspace-root)"
-
-    if [ -n "${ROOT}" ]; then
-        bo="${ROOT}/bazel-bin/$(realpath "--relative-to=${ROOT}" .)"
-    else
-        unset bo
-    fi
-}
-
-function bazel-workspace-output() {
-    if [ -n "${bo}" ]; then
-        cd "${bo}" || return 1
-    else
-        echo 'Not in a bazel workspace'
-        return 1
-    fi
-}
-
 alias b='bazel'
 alias bb='b build'
 alias br='b run'
 alias bt='b test'
-alias bo='bazel-workspace-output'
-
-bazel-workspace-check
