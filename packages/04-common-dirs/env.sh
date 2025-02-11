983e767c9a69157a4d484fec1042439be382836a
Callie Cameron <cjcameron7@gmail.com>
2025-02-11 14:13:17 +0100

    Remove dual boot stuff.

--- packages/04-common-dirs/env.sh
+++ packages/04-common-dirs/env.sh
@@ -1,10 +1,5 @@
 # shellcheck shell=sh
 
-test -n "${WINROOT:-}" && dotfiles-home-link "${WINROOT}" "${HOME}/WinRoot"
-test -n "${WINHOME:-}" && dotfiles-home-link "${WINHOME}" "${HOME}/WinHome"
-test -n "${WINDOWNLOADS:-}" && dotfiles-home-link "${WINDOWNLOADS}" "${HOME}/WinDownloads"
-test -n "${CYGHOME:-}" && dotfiles-home-link "${CYGHOME}" "${HOME}/CygHome"
-
 if [ -z "${DROPBOX}" ] && [ -d "${HOME}/Dropbox" ] && [ ! -h "${HOME}/Dropbox" ]; then
     export DROPBOX="${HOME}/Dropbox"
 else
