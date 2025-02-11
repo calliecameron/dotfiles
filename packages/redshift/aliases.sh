80fae532fce1bdaa37b549cd8dabe667df168860
Callie Cameron <cjcameron7@gmail.com>
2025-02-11 08:33:28 +0100

    Remove redshift; redundant in mint 22.

--- packages/redshift/aliases.sh
+++
@@ -1,5 +0,0 @@
-# shellcheck shell=sh
-
-if [ -z "${DOTFILES_REDSHIFT_LAT}" ] || [ -z "${DOTFILES_REDSHIFT_LON}" ]; then
-    dotfiles-echo-blue 'DOTFILES_REDSHIFT_LAT and DOTFILES_REDSHIFT_LON must be set for redshift to work.'
-fi
