be94b67406d9c9178072a86c79f20ae3a2ae8ada
Callie Cameron <cjcameron7@gmail.com>
2025-04-13 14:59:55 +0200

    Switch to vscodium.

--- packages/vscode/env.sh
+++ packages/vscode/env.sh
@@ -1,4 +1,4 @@
 # shellcheck shell=sh
 
-dotfiles-home-link "${PACKAGE_SOURCE_DIR}/settings.json" "${HOME}/.config/Code/User/settings.json"
-dotfiles-home-link "${PACKAGE_SOURCE_DIR}/keybindings.json" "${HOME}/.config/Code/User/keybindings.json"
+dotfiles-home-link "${PACKAGE_SOURCE_DIR}/settings.json" "${HOME}/.config/VSCodium/User/settings.json"
+dotfiles-home-link "${PACKAGE_SOURCE_DIR}/keybindings.json" "${HOME}/.config/VSCodium/User/keybindings.json"
