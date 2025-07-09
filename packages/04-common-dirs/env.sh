3c53a68e2b43eff61f1b80327cd938a3d6a90d69
Callie Cameron <cjcameron7@gmail.com>
2025-07-09 16:41:17 +0200

    Remove dropbox.

--- packages/04-common-dirs/env.sh
+++
@@ -1,7 +0,0 @@
-# shellcheck shell=sh
-
-if [ -z "${DROPBOX}" ] && [ -d "${HOME}/Dropbox" ] && [ ! -h "${HOME}/Dropbox" ]; then
-    export DROPBOX="${HOME}/Dropbox"
-else
-    dotfiles-home-link "${DROPBOX}" "${HOME}/Dropbox"
-fi
