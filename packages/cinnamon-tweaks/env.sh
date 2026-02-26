ea310742967fc83626c6a6af09a64a34e8d3954c
Callie Cameron <cjcameron7@gmail.com>
2026-02-26 09:49:40 +0100

    Make config more kde-friendly

--- packages/cinnamon-tweaks/env.sh
+++ packages/cinnamon-tweaks/env.sh
@@ -1,4 +1,4 @@
 # shellcheck shell=sh
 
-export DOTFILES_THEME_DARK='Mint-Y-Dark-Aqua'
-export DOTFILES_THEME_LIGHT='Mint-Y-Aqua'
+export DOTFILES_CINNAMON_THEME_DARK='Mint-Y-Dark-Aqua'
+export DOTFILES_CINNAMON_THEME_LIGHT='Mint-Y-Aqua'
