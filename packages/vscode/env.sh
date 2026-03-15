2cea54332d5c82d9712318793ede9b7085248f2c
Callie Cameron <cjcameron7@gmail.com>
2026-03-15 09:50:48 +0100

    Make vscode use gnome keyring

--- packages/vscode/env.sh
+++ packages/vscode/env.sh
@@ -2,3 +2,8 @@
 
 dotfiles-home-link "${PACKAGE_SOURCE_DIR}/settings.json" "${HOME}/.config/VSCodium/User/settings.json"
 dotfiles-home-link "${PACKAGE_SOURCE_DIR}/keybindings.json" "${HOME}/.config/VSCodium/User/keybindings.json"
+
+if env -i which codium >/dev/null; then
+    DOTFILES_ORIGINAL_VSCODE="$(env -i which codium)"
+    export DOTFILES_ORIGINAL_VSCODE
+fi
