7fb2159a345467c6870ab72253c96ad53aeab862
Callie Cameron <callie@calliecameron.com>
2026-04-16 10:40:51 +0200

    Tweak gpg settings

--- packages/misc/env.sh
+++ packages/misc/env.sh
@@ -1,5 +1,6 @@
 # shellcheck shell=sh
 
+dotfiles-home-link "${PACKAGE_SOURCE_DIR}/dirmngr.conf" "${HOME}/.gnupg/dirmngr.conf"
 dotfiles-home-link "${PACKAGE_SOURCE_DIR}/gpg.conf" "${HOME}/.gnupg/gpg.conf"
 dotfiles-home-link "${PACKAGE_SOURCE_DIR}/inputrc"
 dotfiles-home-link "${PACKAGE_SOURCE_DIR}/ssh_config" "${HOME}/.ssh/config"
