3f0c820f8bf7f0e45e364cc04fd7e032ad0749e0
Callie Cameron <callie@calliecameron.com>
2026-04-17 16:09:00 +0200

    Tweak gpg settings

--- packages/misc/env.sh
+++ packages/misc/env.sh
@@ -2,6 +2,7 @@
 
 dotfiles-home-link "${PACKAGE_SOURCE_DIR}/dirmngr.conf" "${HOME}/.gnupg/dirmngr.conf"
 dotfiles-home-link "${PACKAGE_SOURCE_DIR}/gpg.conf" "${HOME}/.gnupg/gpg.conf"
+dotfiles-home-link "${PACKAGE_SOURCE_DIR}/gpg-agent.conf" "${HOME}/.gnupg/gpg-agent.conf"
 dotfiles-home-link "${PACKAGE_SOURCE_DIR}/inputrc"
 dotfiles-home-link "${PACKAGE_SOURCE_DIR}/ssh_config" "${HOME}/.ssh/config"
 dotfiles-home-link "${PACKAGE_SOURCE_DIR}/tmux.conf"
